a649d4ec4785446ef98fe3214505decebe464a25
AlejandroRiveraR <91896449+AlejandroRiveraR@users.noreply.github.com>
2021-10-22 12:12:03 -0500

    ajuste de pruebas unitarias controllers

--- sish/src/test/java/co/com/acueducto/sish/controllers/DominioControllerTest.java
+++ sish/src/test/java/co/com/acueducto/sish/controllers/DominioControllerTest.java
@@ -2,10 +2,16 @@ package co.com.acueducto.sish.controllers;
 
 
 import co.com.acueducto.sish.controllers.configuracion.DominioController;
+import co.com.acueducto.sish.models.auditoria.AuditoriaModel;
 import co.com.acueducto.sish.models.configuracion.DominioModel;
+import co.com.acueducto.sish.repositories.auditoria.AuditoriaRepository;
 import co.com.acueducto.sish.repositories.configuracion.DominioRepository;
+import co.com.acueducto.sish.services.auditoria.AuditoriaService;
+import co.com.acueducto.sish.services.auditoria.IAuditoriaService;
 import co.com.acueducto.sish.services.configuracion.DominioService;
 import co.com.acueducto.sish.services.configuracion.IDominioService;
+import co.com.acueducto.sish.util.utilidades.UtilidadesJSON;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -14,6 +20,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,12 +35,19 @@ public class DominioControllerTest {
   @Mock
   IDominioService idominioService;
 
-  @Mock
-  DominioController controller;
+  DominioController dominioController;
   @Mock
   DominioRepository dominioRepository;
-
   DominioModel dominios;
+  @Mock
+  AuditoriaService auditoriaService;
+  IAuditoriaService iAuditoriaService;
+  @Mock
+  AuditoriaRepository auditoriaRepository;
+  @Mock
+  UtilidadesJSON utilidadesJSON;
+  @Mock
+   BindingResult mockBindingResult;
 
   ArrayList<DominioModel> response = new ArrayList<>();
 
@@ -44,6 +58,9 @@ public class DominioControllerTest {
     MockitoAnnotations.initMocks(this);
     idominioService = new DominioService();
     dominioService = new DominioService();
+    iAuditoriaService = new AuditoriaService();
+    auditoriaService = new AuditoriaService();
+    dominioController = new DominioController();
     dominios = new DominioModel();
     dominios.setDominio("test");
     dominios.setIdDominio(1);
@@ -52,25 +69,57 @@ public class DominioControllerTest {
 
     response.add(dominios);
 
+    AuditoriaModel auditoriaModel = new AuditoriaModel();
+    auditoriaModel.setIdAuditoria(Long.parseLong("1"));
+    auditoriaModel.setId(2);
+    auditoriaModel.setObjeto("dominiosModel");
+    auditoriaModel.setNombreEntidad("ModelosModelo");
+    auditoriaModel.setOperacion(2);
+    Mockito.when(auditoriaRepository.save(auditoriaModel)).thenReturn(auditoriaModel);
+    Mockito.when(utilidadesJSON.convertirObjetoJson(dominios)).thenReturn("");
+    ReflectionTestUtils.setField(auditoriaService,"utilidadesJSON",utilidadesJSON);
+    ReflectionTestUtils.setField(auditoriaService,"auditoriaRepository",auditoriaRepository);
+    ReflectionTestUtils.setField(dominioService,"auditoriaService",auditoriaService);
+
+    Mockito.when(dominioRepository.findByOrderByDominioAsc()).thenReturn(dominioModelList);
+    Mockito.when(dominioRepository.findAll()).thenReturn(dominioModelList);
+    Mockito.when(dominioRepository.save(dominios)).thenReturn(dominios);
+    ReflectionTestUtils.setField(dominioService,"dominioRepository",dominioRepository);
     Mockito.when(dominioService.obtenerPorId(1)).thenReturn(Optional.ofNullable(dominios));
+    Mockito.when(dominioService.actualizar(dominios)).thenReturn(dominios);
     Mockito.when(dominioService.obtener()).thenReturn(dominioModelList);
 
-
-    ReflectionTestUtils.setField(dominioService,"dominioRepository",dominioRepository);
+    ReflectionTestUtils.setField(dominioController,"dominioService",dominioService);
 
   }
 
   @Test
   public void obtener()  {
 
-    List<DominioModel> dominioList=controller.obtener();
 
-
+    List<DominioModel> dominioList=dominioController.obtener();
+    Assertions.assertEquals(dominioList,dominioModelList);
     }
 
   @Test
   public void obtenerPorId()  {
-    Optional<DominioModel> dominioList=controller.obtenerPorId(1);
+
+    Optional<DominioModel> dominioList=dominioController.obtenerPorId(1);
+    DominioModel dominio=dominioList.get();
+    Assertions.assertEquals(dominio,dominios);
+  }
+
+  @Test
+  public void actualizar(){
+    Mockito.when(mockBindingResult.hasErrors()).thenReturn(false);
+    DominioModel dominio = dominioController.actualizar(dominios,mockBindingResult);
+    Assertions.assertEquals(dominio,dominios);
+  }
+  @Test
+  public void crear(){
+    Mockito.when(mockBindingResult.hasErrors()).thenReturn(false);
+    DominioModel dominio = dominioController.crear(dominios,mockBindingResult);
+    Assertions.assertEquals(dominio,dominios);
   }
 
 
